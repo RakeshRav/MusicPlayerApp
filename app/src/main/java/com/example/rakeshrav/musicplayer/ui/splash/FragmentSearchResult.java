69586e323614e47827d3025a4b93e9da6419c4fa
rakeshrav <rakesh.rao291@gmail.com>
2018-08-08 13:38:03 +0530

    main page added

---
+++ app/src/main/java/com/example/rakeshrav/musicplayer/ui/splash/FragmentSearchResult.java
@@ -0,0 +1,67 @@
+package com.example.rakeshrav.musicplayer.ui.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.rakeshrav.musicplayer.R;
+import com.example.rakeshrav.musicplayer.utility.ViewUtils;
+
+import java.util.zip.Inflater;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class FragmentSearchResult extends Fragment {
+
+    private static final String TAG = FragmentSearchResult.class.getSimpleName();
+    @BindView(R.id.llSearchResults)
+    LinearLayout llSearchResults;
+    Unbinder unbinder;
+    private View rootView;
+    int itemCount;
+
+    public static FragmentSearchResult getInstance(int itemCount) {
+        FragmentSearchResult fragmentSearchResult = new FragmentSearchResult();
+        fragmentSearchResult.itemCount = itemCount;
+
+        return fragmentSearchResult;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.frag_search_items, container, false);
+
+        unbinder = ButterKnife.bind(this, rootView);
+
+        setViewInLL();
+        return rootView;
+    }
+
+    private void setViewInLL() {
+
+        llSearchResults.removeAllViews();
+
+        for (int i = 0; i < itemCount; i++){
+            Log.d(TAG,"i = "+i);
+            View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_song, null, false);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewUtils.dpToPx(70));
+            itemView.setLayoutParams(params);
+
+            llSearchResults.addView(itemView);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
--- app/src/main/java/com/example/rakeshrav/musicplayer/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/rakeshrav/musicplayer/ui/splash/SplashActivity.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -28,6 +33,7 @@ import butterknife.OnClick;
 public class SplashActivity extends BaseActivity implements SplashView {
 
     private static final String TAG = SplashActivity.class.getSimpleName();
+    int listSize = 24;
 
     @Inject
     SplashMvpPresenter<SplashView> mPresenter;
@@ -49,6 +55,22 @@ public class SplashActivity extends BaseActivity implements SplashView {
     LinearLayout llMainActivity;
     @BindView(R.id.etSearch)
     EditText etSearch;
+    @BindView(R.id.tvSongsCount)
+    TextView tvSongsCount;
+    @BindView(R.id.llSongsCount)
+    LinearLayout llSongsCount;
+    @BindView(R.id.viewPagerItems)
+    ViewPager viewPagerItems;
+    @BindView(R.id.llIndicators)
+    LinearLayout llIndicators;
+    int numberItemToFit = 0;
+    private SearchResultsPagerAdapter pagerAdapter;
+    private int sizeToolbar = 60;
+    private int sizeSearch = 60;
+    private int sizeCount = 40;
+    private int sizeIndicators = 50;
+    //layout size all in dp
+    private int layoutSize = 70;
 
     public static Intent getStartIntent(Context context) {
         Intent intent = new Intent(context, SplashActivity.class);
@@ -67,8 +89,20 @@ public class SplashActivity extends BaseActivity implements SplashView {
 
         mPresenter.onAttach(this);
 
+        calculateListSize();
         setUp();
+    }
 
+    private void calculateListSize() {
+        int screenSize = ScreenUtils.getScreenHeight(this);
+        int totalSizeOccupied = sizeToolbar + sizeCount + sizeIndicators + sizeSearch;
+
+        int availabelSize = screenSize - ViewUtils.dpToPx(totalSizeOccupied);
+
+        numberItemToFit = availabelSize / ViewUtils.dpToPx(layoutSize);
+
+        Log.d(TAG, "size available for search result : " + availabelSize + ", Items can be fit in that layout : "
+                + numberItemToFit);
     }
 
     @Override
@@ -88,6 +122,9 @@ public class SplashActivity extends BaseActivity implements SplashView {
                 cvSearchSplash.startAnimation(animation);
             }
         }, 2000);
+
+        pagerAdapter = new SearchResultsPagerAdapter(getSupportFragmentManager());
+        viewPagerItems.setAdapter(pagerAdapter);
     }
 
     @OnClick(R.id.cvSearchSplash)
@@ -123,5 +160,21 @@ public class SplashActivity extends BaseActivity implements SplashView {
         llMainActivity.setVisibility(View.VISIBLE);
         Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         llMainActivity.startAnimation(fadein);
+    }
+
+    private class SearchResultsPagerAdapter extends FragmentStatePagerAdapter {
+        public SearchResultsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return FragmentSearchResult.getInstance(numberItemToFit);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
     }
 }
